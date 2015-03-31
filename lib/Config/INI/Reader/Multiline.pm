18897ce52c29b6384c8336ae8ea9107145d286bb
Philippe Bruhat (BooK) <book@cpan.org>
2015-04-01 01:25:19 +0200

    squash whitespace around line continuations into a single space
    to be consistent with Config::INI::Reader decision to trim whitespace
    around keys and values

--- lib/Config/INI/Reader/Multiline.pm
+++ lib/Config/INI/Reader/Multiline.pm
@@ -8,16 +8,16 @@ our @ISA = qw( Config::INI::Reader );
 
 sub parse_value_assignment {
     my ( $self, $line ) = @_;
-    return if $line =~ /\\\s*?\z/;
+    return if $line =~ /\s*\\\s*\z/;
     $line = delete( $self->{__buffer} ) . $line
-        if exists $self->{__buffer};
+        if exists $self->{__buffer} && $line =~ s/^\s*//;
     return $self->SUPER::parse_value_assignment($line);
 }
 
 sub handle_unparsed_line {
     my ( $self, $line, $handle ) = @_; # order changed in CIR 0.024
-    return $self->{__buffer} .= $line
-        if $line =~ s/\\\s*?\z//;
+    return $self->{__buffer} .= "$line "
+        if $line =~ s/\s*\\\s*\z// && $line =~ s/\A\s*//;
     return $self->SUPER::handle_unparsed_line( $line, $handle );
 }
 
